608f8fc55c40ce75c7e5138fe269144947d952d1
Karol Klimaszewski <klimaszewski.karol@gmail.com>
2017-01-09 22:19:10 +0100

    Add movement monster

--- Assets/Potwory/Szkielet/Character/Skeleton.cs
+++ Assets/Potwory/Szkielet/Character/Skeleton.cs
@@ -5,7 +5,15 @@ public class Skeleton : MonoBehaviour
 {
 
     public Transform player;
-    static Animator anim;
+    public Transform head;
+    Animator anim;
+
+    string state = "patrol";
+    public GameObject[] waypoints;
+    int currentWP = 0;
+    public float rotSpeed = 1.0f;
+    public float speed = 1.5f;
+    float accuracyWP = 5.0f;
 
     // Use this for initialization
     void Start()
@@ -17,24 +25,36 @@ public class Skeleton : MonoBehaviour
     void Update()
     {
         var pozycja = GameObject.FindGameObjectWithTag("Wizzard").transform.position;
-
-
         Vector3 direction = pozycja - this.transform.position;
-        float angle = Vector3.Angle(direction, this.transform.forward);
+        direction.y = 0;
+        float angle = Vector3.Angle(direction, head.up);
 
-        // Dodanie ze potwor widzi nas jak jestesmy z jego przodu.
-        //if (Vector3.Distance(pozycja, this.transform.position) < 10 && angle < 30)
-        //float dist = Vector3.Distance(pozycja, this.transform.position);
-   
-        if (Vector3.Distance(pozycja, this.transform.position) < 10)
+        if (state == "patrol" && waypoints.Length > 0)
         {
-            direction.y = 0;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
-
             anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            if(Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
+            {
+                //currentWP = Random.Range(0, waypoints.Length);
+                currentWP++;
+                if(currentWP >= waypoints.Length)
+                {
+                    currentWP = 0;
+                }
+            }
+            direction = waypoints[currentWP].transform.position - transform.position;
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+            this.transform.Translate(0, 0, Time.deltaTime * speed);
+        }
+
+        if (Vector3.Distance(pozycja, this.transform.position) < 10 && (angle < 30 || state == "pursuing"))
+        {
+            state = "pursuing";
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+
             if (direction.magnitude > 2)
             {
-                this.transform.Translate(0, 0, 0.05f);
+                this.transform.Translate(0, 0, Time.deltaTime * speed);
                 anim.SetBool("isWalking", true);
                 anim.SetBool("isAttacking", false);
             }
@@ -46,9 +66,9 @@ public class Skeleton : MonoBehaviour
         }
         else
         {
-            anim.SetBool("isIdle", true);
-            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalking", true);
             anim.SetBool("isAttacking", false);
+            state = "patrol";
         }
     }
 }
