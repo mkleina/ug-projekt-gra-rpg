01b76e4f4ad5be9f49420df5439953ac12adcf1d
kirin1994 <apodlawski@gmail.com>
2017-01-14 06:06:38 +0100

    Multi in progres TO DO : Animation Serialize

---
+++ Assets/Network/NetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+    const string VERSION = "v0.0.1";
+    public string roomName = "Dungeon";
+    public string playerPrefab = "Wizard";
+    public Transform spawnPoint;
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+	}
+	
+	// Update is called once per frame
+	void OnJoinedLobby()
+    {
+        RoomOptions roomOprions = new RoomOptions() { IsVisible = false, maxPlayers = 4 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOprions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation, 0);
+    }
+}
---
+++ Assets/Network/NetworkPlayer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+    public GameObject camera;
+    Vector3 position;
+    Quaternion rotation;
+    bool isAlive = true;
+    float lerp = 5f;
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.sendRate = 20;
+        PhotonNetwork.sendRateOnSerialize = 10;
+        if (photonView.isMine)
+        {
+            camera.SetActive(true);
+            GetComponent<CameraMove>().enabled = true;
+            GetComponent<WSAD>().enabled = true;
+        }
+        else
+        {
+            StartCoroutine("Alive");
+        }
+    }
+
+    // Update is called once per frame
+    void OnPhotonSerializeVIew(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            //position = (Vector3)stream.ReceiveNext();
+            //rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+    //IEnumerator Alive()
+    //{
+    //    while(isAlive)
+    //    {
+    //        transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * lerp);
+    //        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * lerp);
+    //        yield return null;
+    //    }
+    //}
+}
