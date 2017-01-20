4f618d808e0423dc36a4e5827df6371c537e6e62
Maciej Marzec <dzyzusek@gmail.com>
2017-01-20 22:15:21 +0100

    Add world night lightning, animals and cleanup hierarchy

---
+++ Assets/DNCycle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;  
+using System.Collections;                                            //  --1--
+
+public class DNCycle : MonoBehaviour {
+
+	public float speed = 1; 
+	private float mn;                                            // --2--
+
+	void Update () {                                             // --3--
+		mn = speed * Time.deltaTime;                         // --4--
+		transform.Rotate (Vector3.right * mn);               // --5--
+	}
+}
---
+++ Assets/Zajonc.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var zajoncclip:AudioClip;
+var isZajonc = true;
+
+
+
+
+function OnTriggerEnter(o:Collider){
+	Debug.Log("Na wejscie");
+
+	 if(isZajonc == true){
+	 	playKappa();
+	 }
+
+
+}
+
+function playKappa(){
+	 GetComponent(AudioSource).PlayOneShot(zajoncclip, 1.0);
+	 isZajonc = false;
+
+}
